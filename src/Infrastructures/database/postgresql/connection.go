7b781f7c656022a80db8adbfe5f9ed6efdf5f0c2
vedoalfarizi <vedoalfarizi@gmail.com>
2022-09-20 16:14:34 +0700

    update db conn

--- src/Infrastructures/database/postgresql/connection.go
+++ src/Infrastructures/database/postgresql/connection.go
@@ -9,7 +9,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=wecan password=1sampai8 sslmode=disable TimeZone=Asia/Jakarta")
+	database, err := gorm.Open("postgres", "host=localhost port=5492 user=root dbname=wecan password=pass sslmode=disable TimeZone=Asia/Jakarta")
 	if err != nil {
 		panic(err)
 	}
